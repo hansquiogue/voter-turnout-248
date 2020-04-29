8f123114df8da51b2d9b16d7f7103a8791ead9b1
Hans Quiogue <hansquiogue@gmail.com>
2020-04-29 15:52:36 -0400

    Revised Shiny code annd implemented more functionality. This includes different plots of either voter or registration data, or both combined, and an implementation of a table that can be filtered based on user input.

--- shiny-voting/app.R
+++ shiny-voting/app.R
@@ -1,99 +1,31 @@
 library(shiny)
 library(tidyverse)
-library(readxl)
 library(DT)
-library(reshape2)
-
-# Apologies for this garbage, will be heavily revised later
-
-# TODO: Perhaps put data wrangling into a seperate R script 
-# to write into a csv file and read into this Shiny app to
-# avoid current code clutter
-
-# === Wrangling data ==== #
-# Uncleaned historical data
-hist_df <- read_xlsx("a1.xlsx", skip = 5, na = "NA")
-# Different voting category names to put into columns for the dataframe
-voting_col_names <- c("Year", "Population-Count", "Population-Percent",
-                      "Citizen-Percent", "White-Percent", "Citizen-White-Percent", 
-                      "White-Non-Hispanic-Percent", "Citizen-White-Non-Hispanic-Perc", 
-                      "Black-Percent", "Citizen-Black-Percent", "Asian-Percent",
-                      "Citizen-Asian-Percent", "Hispanic/Other-Percent",
-                      "Citizen-Hispanic/Other-Perc", "Male-Percent", "Female-Percent")
-# Assigns proper column names 
-colnames(hist_df) <- voting_col_names
-# Converts all columns to numeric
-hist_df[] <- lapply(hist_df, function(x) as.numeric(x))
-# Converts year column to a categorical value
-hist_df$Year <- as.factor(hist_df$Year)
-
-# 1964 to 2018 (Goes by every 2 year)
-years_length <- (2018 - 1964) / 2
-# List of proper age groups and voting status to put into dataframe
-age_groups <- c("Total", "18 to 24", "25 to 44", "45 to 64", "65+")
-vote_status <- c("Voted", "Registered")
-
-temp_df <- data.frame()
-
-# Integer values representing current index values of corresponding list
-curr_status <- 1
-curr_age <- 1
-
-# Loop to cleans up hist_df (Loop increments are added by 6 to account for missing columns)
-for(i in seq(1, nrow(hist_df), years_length + 6)) {
-    # Appends new rows to temp_df from subset of hist_df
-    if(i != 1) temp_df <- bind_rows(temp_df, hist_df[(i - 1):((i - 1) + years_length), ])
-    else temp_df <- bind_rows(temp_df, hist_df[i:(i + years_length), ])
-}
-
-# Appends new columns for age groups and voting statuses
-temp_df <- data.frame(append(temp_df, c(Age = age_groups[curr_status], 
-                                        Status = vote_status[curr_age]),
-                             after = 0), stringsAsFactors = FALSE)
-
-temp_index = 1
-# Loop that corrects age group and voting status rows
-for(i in 1:(length(age_groups) * length(vote_status))) {
-    temp_df$Age[temp_index:(temp_index + years_length)] <- age_groups[curr_age]
-    temp_df$Status[temp_index:(temp_index + years_length)] <- vote_status[curr_status]
-    temp_index <- (temp_index + years_length) + 1
-    # Updates age group and voting status to match rows
-    if(curr_status == 1) curr_status = curr_status + 1
-    else {
-        curr_status = 1
-        curr_age = curr_age + 1
-    }
-}
-# Removes unnessary rows at the nd
-hist_df <- temp_df[1:279, ]
-
-hist_df$Age <- as.factor(hist_df$Age)
-hist_df$Status <- as.factor(hist_df$Status)
-
-# === Shiny code ==== #
 
 ui <- fluidPage(
 
-    h3(titlePanel("Voting Project Title")),
+    h3(titlePanel("U.S. Voting Turnout by Age Groups")),
     
     sidebarLayout(
         sidebarPanel(
-            # Main inputs (Can change from historical to state to education data)
+            # Main inputs (Can change from historical to different years, etc)
             selectInput("main_inputs", h3("Data Selected:"), 
                         choices = list("Historical" = 1, 
-                                       "Etc." = 2),
+                                       "2018" = 2),
                         selected = 1),
-            checkboxGroupInput("hist_ages", "Choose an age group:",
-                               choices = list("Total" = 1,"Young" = 2,
-                                              "Adult" = 3,"Mid Age" = 4,
-                                              "Elderly" = 5),
-                               selected = c(1, 2, 3, 4, 5)),
-            radioButtons("hist_status", "Choose voting status:",
-                         choices = list("Voted" = 1, "Registered" = 2),
-                         selected = 1)
-            
-            ),
-
+            # If historical is chosen, this layout will be displayed
+            conditionalPanel(
+                condition = "input.main_inputs  == '1'",
+                checkboxInput("hist_comb", "Combine votes/registration data", TRUE),
+                checkboxGroupInput("hist_ages", "Choose an age group:",
+                                    choices = list("Total" = 1,"Young" = 2,
+                                                    "Adult" = 3,"Mid Age" = 4,
+                                                    "Elderly" = 5),
+                                    selected = c(1, 2, 3, 4, 5)),
+                radioButtons("hist_status", "Choose voting status:",
+                             choices = list("Voted" = 1, "Registered" = 2),
+                             selected = 1))),
+     
         mainPanel(
            plotOutput("hist_plots"),
            dataTableOutput("hist_table"),
@@ -102,39 +34,56 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+    
+    # hist_df is clean dataframe of historical voting data
+    hist_df <- read.csv("clean_hist.csv")
+    age_groups <- c("Total", "18 to 24", "25 to 44", "45 to 64", "65+")
+    vote_status <- c("Voted", "Registered")
+    
     output$hist_plots <- renderPlot({
+        # Dataframe of filtered and selected age groups from user input
+        plot_df <- hist_df[hist_df$Age %in% age_groups[as.numeric(input$hist_ages)], ] 
         # Historical plot will be used
         if(input$main_inputs == 1) {
-            # Filtered age categories to use in plot
-            select_category <- c()
-            cnt = 1
-            # Loop to check what categories should be used based on input
-            for(i in 1:length(age_groups)) {
-                if(i %in% input$hist_ages) {
-                    select_category[cnt] <- age_groups[i]
-                    cnt <- cnt + 1
-                }
+            # Historical plot of combined votes and registration data
+            if(input$hist_comb == TRUE) {
+                ggplot(data = plot_df) +
+                    aes(Year, Population.Count, group = Age, color = Age) + 
+                    geom_point() + geom_line() + 
+                    ggtitle("Votes and Registration Between Age Groups (1964-2018)") +
+                    ylab("Total Population (In Thousands)") + 
+                    xlab("Years (1964-2018)") + 
+                    theme_minimal() +
+                    theme(axis.text.x = element_blank())
             }
-            # Plot of filtered and selected age groups 
-            plot_df <- hist_df[hist_df$Age %in% select_category, ]
-            
-            ggplot(data = plot_df) +
-                aes(Year, Population.Count, group = Age, color = Age) + 
-                geom_point() + geom_line() + 
-                ggtitle("Voting Between Young People Compared to Other Age Groups (1964-2018)") +
-                ylab("Total Population (In Thousands)") + 
-                xlab("Years (1964-2018)") + 
-                theme_minimal() +
-                theme(axis.text.x = element_blank())
+            # Historical plot of either votes and registration data
+            else {
+                plot_df <- plot_df %>% filter(Status == vote_status[as.numeric(input$hist_status)])
+                # Historical plot
+                ggplot(data = plot_df) +
+                    aes(Year, Population.Percent, group = Age, color = Age) + 
+                    geom_point() + geom_line() + 
+                    ggtitle("Vote/Registration Percentage Between Age Groups (1964-2018)") +
+                    ylab("Percentage") + 
+                    xlab("Years (1964-2018)") + 
+                    theme_minimal() +
+                    theme(axis.text.x = element_blank())
+            }
         }
     })
     
     output$hist_table <- renderDataTable({
         if(input$main_inputs == 1) {
-            hist_df %>% 
-                filter(Age == "Total" & Status == "Voted") %>% 
-                select(Year, Population.Count) %>% 
-                rename(`Pop. Count (In Thousands)` = Population.Count)
+            # Dataframe of filtered and selected age groups from user input
+            table_df <- hist_df[hist_df$Age %in% age_groups[as.numeric(input$hist_ages)], ]
+            select_vote <- as.numeric(input$hist_status)
+            # Table of historical data
+            table_df %>% 
+                filter(Status == vote_status[select_vote]) %>% 
+                mutate(Population.Count = (Population.Count * 1000)) %>% 
+                select(Year, Age, Population.Percent, Population.Count) %>%
+                rename(`Population Count Voted and Registered` = Population.Count) %>% 
+                rename(`Voted/Registered Percentage` = Population.Percent)
         }
     })
 }
